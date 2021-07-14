ac4139b744630fc4a62855113983aadee1d1f3e1
Vasiuchenko <vasiuchenko@icloud.com>
2021-07-14 14:52:57 +0300

    Create an array with filtered strings

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const fs = require('fs');
 
+// Read file and form an array
+
 let strings = fs.readFileSync('./sourse.txt'),
-    fibonacci = [];
+    fibonacci = [],
+    newString = [];
 
 strings = strings.toString();
-strings = strings.split("\n");
+strings = strings.split("\r\n");
 
-console.log(strings.length)
 if (strings.length > 0) fibonacci = createFibonacci(strings.length);
 
+// Create an array with filtered strings
+for (let i = 0; i < fibonacci.length; i++) {
+    let string = strings.filter((item, index) => index === fibonacci[i]);
+    
+    if(typeof string[0] === 'string') newString[i] = string[0];
+}
 
-
-
+//  Fibonacci sequence
 function createFibonacci(length) {
     let arr = [0, 1];
 
     for (let i = 0; i < arr.length; i++) {
         let item = arr[i] + arr[i + 1];
+        
         if (length <= arr[arr.length - 1]) break;
         arr.push(item);
     }
+    arr = [...new Set(arr)];
     return arr;
 }
+
+
+
+console.log(newString)
